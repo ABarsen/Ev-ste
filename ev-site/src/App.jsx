f1b9bef5ff95f8e3d2479730ec63f6be47c9ae04
ABarsen <kiplagatemmanuel21@gmail.com>
2024-11-09 09:22:09 +0300

    removed comments

--- ev-site/src/App.jsx
+++ ev-site/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 import Background from "./components/Background/Background";
 import NavBar from "./components/NavBar/NavBar";
 // import Hero from "./components/Hero/Hero";
-const App = () => {
 
+const App = () => {
   const heroData = [
-    {text1: "Dive into",text2: "What you love"},
-    {text1: "Indulge",text2: "Your Passion"},
-    {text1: "Give in to ",text2: "Your passions"},
-  ]
-  
-  const [heroCount, setHeroCount]= useState (0);
-  const [playStatus, setPlayStatus] =useState (false);
+    { text1: "Dive into", text2: "What you love" },
+    { text1: "Indulge", text2: "Your Passion" },
+    { text1: "Give in to", text2: "Your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(true);
+
   return (
     <div>
-    <NavBar/>
-      <Background playStatus = {playStatus} heroCount = {heroCount}/>
-     {/* this hero component was giving me an error */}
-     {/* <Hero 
-     setPlayStatus={setPlayStatus}
-     heroData ={heroData [heroCount]}
-     heroCount ={heroCount}
-     setHeroCount ={setHeroCount}
-     playStatus ={playStatus}
-     /> */}
-
+      <NavBar />
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      {/* If you want to use the Hero component, uncomment both the import and the component below */}
+      {/* <Hero 
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      /> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
