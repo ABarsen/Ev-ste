b3568ca3398542560a8ee18043bdfa948ba8476f
ABarsen <kiplagatemmanuel21@gmail.com>
2024-11-26 16:39:42 +0300

    improved navbar

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Background from "./components/Background/Background";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+
+const App = () => {
+  const heroData = [
+    { text1: "Dive into", text2: "What you love" },
+    { text1: "Indulge", text2: "Your Passion" },
+    { text1: "Give in to", text2: "Your passions" },
+  ];
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  return (
+    <div>
+      <NavBar />
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      {/* If you want to use the Hero component, uncomment the component below */}
+      <Hero 
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+    
+  );
+
+};
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,66 @@
+import "./Hero.css";
+import PropTypes from "prop-types";
+import arrow_btn from "../../assets/arrow_btn.png";
+import play_icon from "../../assets/play_icon.png";
+import pause_icon from "../../assets/pause_icon.png";
+
+const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) => {
+  return (
+    <div className="hero">
+      <div className="herotext">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore the features</p>
+        <img src={arrow_btn} alt="" />
+      </div>
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li
+            onClick={() => setHeroCount(0)}
+            className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(1)}
+            className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(2)}
+            className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+        </ul>
+        <div className="hero-play">
+          <img
+            onClick={() => setPlayStatus(!playStatus)}
+            src={playStatus ? pause_icon : play_icon}
+            alt=" click me"
+          />
+          <p>see the video</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  heroData: PropTypes.shape({
+    text1: PropTypes.string.isRequired,
+    text2: PropTypes.string.isRequired,
+  }).isRequired,
+  setHeroCount: PropTypes.func.isRequired,
+  heroCount: PropTypes.number.isRequired,
+  setPlayStatus: PropTypes.func.isRequired,
+  playStatus: PropTypes.bool.isRequired,
+};
+
+Hero.defaultProps = {
+  heroData: {
+    text1: " Lets dive in",
+    text2: " let get your Ev ",
+  },
+  heroCount: 0,
+  playStatus: false,
+};
+
+export default Hero;
